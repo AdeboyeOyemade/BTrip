5571f04866306b3173a7ce559dc17bdec03fa4e8
Adeboye Oyemade <91950243+Adeholmes@users.noreply.github.com>
2021-10-27 13:37:31 +0000

    Day 1

---
+++ day1.py
@@ -0,0 +1,7 @@
+print("welcome to my band name crew")
+cityname = input("what city did you grow up"\n)
+petname = input("what is your pet's name"\n)
+print("your bandname is ")
+bandname = cityname + petname
+print(bandname)
+
